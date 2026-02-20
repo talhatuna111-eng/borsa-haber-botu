eb1dc25e34a34eb551cb55cfaaaec209f946f13e
talhatuna111-eng <talhatuna111@gmail.com>
2026-02-21 02:39:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/2965/2965879.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/2965/2965879.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
